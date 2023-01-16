68faf8349bfab2de02de2307560a72e335c192a9
ahamadi <aziz.hamadi@advyteam.com>
2023-01-16 23:23:12 +0100

    add UIImageView extension o load image from presignedUrl

---
+++ StepperView/Extension/UIImageView.swift
@@ -0,0 +1,29 @@
+//
+//  UIImageView.swift
+//  StepperView
+//
+//  Created by Aziz Hamadi on 16/1/2023.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(from link: String) {
+        guard let url = URL(string: link) else { return }
+        load(from: url)
+    }
+    func load(from url: URL) {
+        URLSession.shared.configuration.timeoutIntervalForRequest = .infinity
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+}
